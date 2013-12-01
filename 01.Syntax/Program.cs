67591af6941ebdbd8d4776ac3b52b65cc4ea5bd9
Victor Grigoriu <vgrigoriu@gmail.com>
2013-12-01 20:22:26 +0200

    First project in solution

---
+++ 01.Syntax/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Syntax
+{
+    class Program
+    {
+        static void Main()
+        {
+            var my = new MyClass();
+            my.PrintA();
+        }
+    }
+
+    class MyClass
+    {
+        private int a;
+
+        internal void PrintA()
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
