254e47c93e852b14217505ce77e2bb097cd5a798
Victor Grigoriu <vgrigoriu@gmail.com>
2013-12-01 21:25:45 +0200

    Moștenire, virtualități

---
+++ 02.Classes/AbstractBaseClass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Classes
+{
+    // clasele abstracte au membri abstracți
+    abstract class AbstractBaseClass
+    {
+        // funcție abstractă, trebuie suprascrisă
+        // de sub-clasele concrete
+        protected abstract void PrintInnerMessage();
+
+        // funcție virtuală, poate fi suprascrisă
+        // de subclase
+        protected virtual void PrintMessageWithDefault()
+        {
+            Console.WriteLine("Message in ABC");
+        }
+
+        // Method Template
+        // By default, metodele sunt non-virtuale
+        public void PrintMessage()
+        {
+            Console.WriteLine("Incipit vita brevis");
+            PrintInnerMessage();
+            PrintMessageWithDefault();
+            Console.WriteLine("Gata.");
+        }
+    }
+
+    // nu mai e abstractă
+    class BaseClass : AbstractBaseClass
+    {
+        // pentru că implementează toate metodele abstracte
+        // din clasa de bază
+        protected override void PrintInnerMessage()
+        {
+            Console.WriteLine("This is the base class");
+        }
+    }
+
+    class OneDerivedClass: BaseClass
+    {
+        // suprascrie metoda virtuală, nu face nimic
+        protected override void PrintInnerMessage()
+        {
+        }
+
+        // suprascrie metoda virtuală, face altceva
+        protected override void PrintMessageWithDefault()
+        {
+            Console.WriteLine("Message in one derived class");
+        }
+    }
+
+    class OtherDerivedClass : BaseClass
+    {
+        // suprascrie metoda virtuală, apelează și
+        // metoda suprascrisă
+        protected override void PrintMessageWithDefault()
+        {
+            base.PrintMessageWithDefault();
+            Console.WriteLine("Message in other derived class");
+        }
+    }
+
+    class ExtraDerivedClass : BaseClass
+    {
+        // Dacă faci override la o metodă ne-virtuală, nu apare
+        // comportamentul polimorfic.
+        // se poate și fără new, dar nu e indicat, că e derutant
+        public new void PrintMessage()
+        {
+            Console.WriteLine("I'm extra.");
+        }
+    }
+}
--- 02.Classes/Program.cs
+++ 02.Classes/Program.cs
@@ -9,12 +9,14 @@ namespace Classes
     {
         static void Main()
         {
+            // normal
             NormalClass.PrintStaticMessage();
 
             var normalObject = new NormalClass("vaca");
 
             normalObject.PrintValues();
 
+            // advanced
             AdvancedClass.PrintHowManyInstances();
             var advancedObject = new AdvancedClass();
             AdvancedClass.PrintHowManyInstances();
@@ -25,6 +27,48 @@ namespace Classes
 
             advancedCopy.Dispose();
             advancedObject.Dispose();
+
+            // polymorphism
+            AbstractBaseClass abcOneDerived = new OneDerivedClass();
+            var oneDerived = new OneDerivedClass();
+
+            abcOneDerived.PrintMessage();
+            oneDerived.PrintMessage();
+
+            AbstractBaseClass abcOtherDerived = new OtherDerivedClass();
+            var otherDerived = new OtherDerivedClass();
+
+            abcOtherDerived.PrintMessage();
+            otherDerived.PrintMessage();
+
+            AbstractBaseClass abcExtra = new ExtraDerivedClass();
+            var extra = new ExtraDerivedClass();
+
+            abcExtra.PrintMessage();
+            extra.PrintMessage();
+
+            // casting
+            object o = abcOneDerived;
+            // o să arunce InvalidCastException:
+            //var s = (string)o;
+            // returnează null, nu aruncă excepție:
+            var s = o as string;
+
+            Console.WriteLine(
+                "o is OneDerivedClass: {0}",
+                o is OneDerivedClass);
+            Console.WriteLine(
+                "o is BaseClass: {0}",
+                o is BaseClass);
+            Console.WriteLine(
+                "o is AbstractBaseClass: {0}",
+                o is AbstractBaseClass);
+            Console.WriteLine(
+                "o is object: {0}",
+                o is object);
+            Console.WriteLine(
+                "o is string: {0}",
+                o is string);
         }
     }
 }
