4d1443414fd38b84ce3ee9300505557c2333bf4f
Victor Grigoriu <vgrigoriu@gmail.com>
2014-10-27 20:50:58 +0200

    Add exceptions project

---
+++ 07.Exceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _07.Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new ThrowingClass();
+
+            o.ThrowIf9(123);
+
+            try
+            {
+                o.ThrowIf9(9);
+            }
+            catch (ArgumentException argumentException)
+            {
+                Console.WriteLine(
+                    "Caught something: {0}\nCall stack: {1}",
+                    argumentException.Message,
+                    argumentException.StackTrace);
+            }
+            // don't do this
+            catch (Exception exception)
+            {
+
+            }
+
+            // finally
+
+            // using
+
+            // create your own exception
+        }
+    }
+}
---
+++ 07.Exceptions/ThrowingClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07.Exceptions
+{
+    class ThrowingClass
+    {
+        internal void ThrowIf9(int number)
+        {
+            Console.WriteLine("First message for {0}", number);
+
+            if (number == 9)
+            {
+                throw new ArgumentException("it was nine", "number");
+            }
+
+            Console.WriteLine("Second message for {0}", number);
+        }
+    }
+}
