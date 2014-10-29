7e5b38e4d67edb207550067a0d7c28e954bf17d0
Victor Grigoriu <vgrigoriu@gmail.com>
2014-10-29 22:51:12 +0200

    Async project: use a Task

---
+++ 11.Async/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+namespace _11.Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var file = File.Open(@"..\..\Program.cs", FileMode.Open))
+            using (var reader = new StreamReader(file))
+            {
+                var lineTask = reader.ReadLineAsync();
+                // do other stuff
+                BigInteger x = 0;
+                while (!lineTask.IsCompleted)
+                {
+                    x = x + 1;
+                }
+
+                var line = lineTask.Result;
+                Console.WriteLine("Am ajuns la {0}, am citit {1}", x, line);
+            }
+        }
+    }
+}
